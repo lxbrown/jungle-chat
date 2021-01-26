dcc766986446f7ddffa961cb85d0046e6f40caee
Alex Brown <lxbrown13@gmail.com>
2021-01-25 20:30:26 -0500

    Implement basic launch page

--- client/src/Launch.tsx
+++ client/src/Launch.tsx
@@ -1,3 +1,19 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 export default function Launch() {
-  return (<></>)
+  const [channel, setChannel] = useState('');
+
+  const handleOnInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChannel(event.target.value)
+  }
+
+  return (
+    <div className="container">
+      <input type="text" placeholder="Channel" value={channel} onChange={handleOnInputChange} className="channel-input" />
+      <Link to={`/${channel}`} className="join-link">
+        Join channel
+      </Link>
+    </div>
+  )
 }
