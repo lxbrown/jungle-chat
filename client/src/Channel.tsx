7599df89fd49b5409ad63f451d604b2f75103e2a
Alex Brown <lxbrown13@gmail.com>
2021-01-25 21:11:23 -0500

    Add (transient) message history

--- client/src/Channel.tsx
+++ client/src/Channel.tsx
@@ -5,21 +5,51 @@ export interface MatchParams {
   channel: string;
 }
 
+export interface Message {
+  body: string;
+  user: string;
+  currentUser: boolean;
+}
+
 export default function Channel() {
   const match = useRouteMatch<MatchParams>('/:channel');
-  const channel = match?.params.channel ? match?.params.channel : 'Unknown';
-  const [input, setInput] = useState('');
+  const [messageText, setMessageText] = useState('');
+  const [messages, setMessages] = useState<Message[]>([]);
 
-  const handleOnInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInput(event.target.value);
+  const handleOnMessageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMessageText(event.target.value);
+  }
+
+  const handleSend = () => {
+    sendMessage(messageText);
+    setMessageText('');
+  }
+
+  function sendMessage(text: string) {
+    const message: Message = {
+      body: text,
+      user: '',
+      currentUser: true
+    }
+    setMessages(messages => [...messages, message]);
+    return;
   }
 
   return (
     <div className="channel-container">
-      <h3>{channel}</h3>
+      <h3>{match?.params.channel ? match?.params.channel : 'Unknown'}</h3>
+      <div className="message-container">
+        <ol className="messages">
+          {messages.map((message, i) => (
+            <li key={i} className={`message-item ${message.currentUser ? "sent-message" : "received-message"}`}>
+              {message.body}
+            </li>
+          ))}
+        </ol>
+      </div>
 
-      <input type="text" placeholder="Message" value={input} onChange={handleOnInputChange} className="message-input" />
-      <button className="send-button">
+      <input type="text" placeholder="Message..." value={messageText} onChange={handleOnMessageChange} className="message-input" />
+      <button className="send-button" onClick={handleSend}>
         Send
       </button>
     </div>
