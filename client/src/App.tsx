00d0fefbdb5c71998d8e7eb56bf1632058a0ef3e
Alex Brown <lxbrown13@gmail.com>
2021-01-25 20:20:11 -0500

    Add routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,19 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Launch from './Launch';
+import Channel from './Channel';
 
-function App() {
-  return (<></>);
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Launch />
+        </Route>
+        <Route exact path="/:channel">
+          <Channel />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
-
-export default App;
