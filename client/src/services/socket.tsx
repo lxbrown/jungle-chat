5151adf3e0bca71b5e88c99772e6c31109a6f043
Alex Brown <lxbrown13@gmail.com>
2021-02-01 20:43:41 -0500

    remove socket server URL from client

--- client/src/services/socket.tsx
+++ client/src/services/socket.tsx
@@ -1,5 +1,5 @@
 import { io, Socket } from "socket.io-client";
 
-const SOCKET_SERVER_URL = "";
+const SOCKET_SERVER_URL = "http://localhost:4000";
 
-export const socket: Socket = io(SOCKET_SERVER_URL);
+export const socket: Socket = io();
