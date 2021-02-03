99537505ac8201299ab4dabb93cea0bff7eca56f
Alexander Brown <lxbrown13@gmail.com>
2021-02-03 15:07:17 -0500

    Add functional local/remote mongodb connection

---
+++ server/src/connections/dbConnection.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+module.exports = mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  auth: {
+    user: process.env.MONGO_USERNAME,
+    password: process.env.MONGO_PASSWORD
+  }
+})
