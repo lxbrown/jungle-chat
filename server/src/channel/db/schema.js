592f7b91706ded864a94f1a0d97d051becae694d
Alexander Brown <lxbrown13@gmail.com>
2021-02-05 15:09:16 -0500

    Clarify future work items

--- server/src/channel/db/schema.js
+++ server/src/channel/db/schema.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = new mongoose.Schema({             //Metadata about the channel
-    display_name: String,                               //Name to display on screen
-    description: String,                                //Description of channel
+    display_name: String,                          //Name to display on screen
+    description: String,                           //Description of channel
+    persistent: Boolean,                           //Do we persist messages for this channel, or is it transient
 });
