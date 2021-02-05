23286b0cbd6dc7fce741a3b30b985e018b87a634
Alexander Brown <lxbrown13@gmail.com>
2021-02-05 09:08:56 -0500

    Add temp API to get message history for a channel

--- server/src/index.js
+++ server/src/index.js
@@ -8,14 +8,27 @@ const app = express();
 const server = http.createServer(app);
 
 const dbConn = require('./connections/dbConnection');
+const Message = require('./models/message');
 require('./connections/socketConnection')(server);
 
 //Serve static files from client build
 const UI_BUILD = path.join(__dirname, '..', '..', 'client', 'build');
 app.use(express.static(UI_BUILD));
 
+//TODO: export routes from the Chat package, and import them here as "app.use('/api', Routes())"
+app.get('/api/channel/:channel/message', (req, res) =>{
+  Message.find({
+    channel_id: req.params.channel
+  }).
+  limit(10).
+  sort('+created_at'), (err, messages) => {
+    res.json(messages);
+  };
+  // res.json({'hi':true})
+});
+
 //Forward all paths to client for routing
-app.get('*', (req,res) =>{
+app.get('*', (req, res) =>{
   res.sendFile(path.join(UI_BUILD, 'index.html'));
 });
 
