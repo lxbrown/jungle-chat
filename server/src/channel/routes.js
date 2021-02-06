5364edd06aa8b22e26b477b6cdb35d5d86b4dbf3
Alex Brown <lxbrown13@gmail.com>
2021-02-06 14:12:14 -0500

    Disable API for creating new channels

--- server/src/channel/routes.js
+++ server/src/channel/routes.js
@@ -11,14 +11,16 @@ const Routes = () => {
       res.json(err);
     });
   })
-  router.route('').post((req, res, next) => {
-    const { short_name, display_name, description, persistent } = req.body;
-    interactions.createChannel(short_name, display_name, description, persistent).then(channel => {
-      res.json(channel);
-    }, err => {
-      res.json(err);
-    });
-  })
+  // Disabling the ability to create channels via an HTTP request. For
+  // now, those can only be created internally.
+  // router.route('').post((req, res, next) => {
+  //   const { short_name, display_name, description, persistent } = req.body;
+  //   interactions.createChannel(short_name, display_name, description, persistent).then(channel => {
+  //     res.json(channel);
+  //   }, err => {
+  //     res.json(err);
+  //   });
+  // })
 
   return router;
 }
